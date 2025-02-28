cafc78270163afc5c6f85a1931929a325fcac377
Ali Meherremov <150355933+AliMeherremov@users.noreply.github.com>
2025-02-28 18:16:44 +0300

    Add files via upload

---
+++ Hafta3_Odev1/Hafta3_Odev1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Bir sayı giriniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine()); 
+int toplam = 0;
+
+while (sayi != 0) 
+{
+    toplam = toplam + (sayi % 10); 
+    sayi = sayi / 10; 
+}
+
+Console.WriteLine("Rakamların toplamı: " + toplam);
